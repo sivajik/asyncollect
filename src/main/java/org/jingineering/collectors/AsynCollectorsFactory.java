f409a913ab24ebcaeeac22318a69dfac8fe1f09f
sivajik <sivaji_kondapalli@yahoo.com>
2025-06-02 14:03:53 +0100

    small refactoring

--- src/main/java/org/jingineering/collectors/AsynCollectorsFactory.java
+++ src/main/java/org/jingineering/collectors/AsynCollectorsFactory.java
@@ -2,15 +2,16 @@ package org.jingineering.collectors;
 
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Executor;
-import java.util.concurrent.Executors;
 import java.util.function.Function;
 import java.util.stream.Collector;
 import java.util.stream.Stream;
 
+import static java.util.concurrent.Executors.newVirtualThreadPerTaskExecutor;
+
 public final class AsynCollectorsFactory {
     static <T, R, RR> Collector<T, ?, CompletableFuture<RR>> collecting(Function<Stream<R>, RR> finalizer,
                                                                         Function<? super T, ? extends R> mapper) {
-        return AsynParallelCollector.from(mapper, finalizer, Executors.newVirtualThreadPerTaskExecutor());
+        return AsynParallelCollector.from(mapper, finalizer, newVirtualThreadPerTaskExecutor());
     }
 
     static <T, R, RR> Collector<T, ?, CompletableFuture<RR>> collecting(Function<Stream<R>, RR> finalizer,
